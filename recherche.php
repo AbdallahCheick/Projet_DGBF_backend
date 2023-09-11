1ca23b6c106c09e4fb3b19f55d850e9e2f33c00e
Cheick Abdallah <cheickabdallahtanagueda@gmail.com>
2023-09-11 17:19:51 +0000

    Projet final

--- recherche.php
+++ recherche.php
@@ -1,50 +1,103 @@
 <?php
-    include('connexion.php');
-    if (isset($_POST['rechercher'])) {
-        $date_heure_debut = $_POST["date_heure_debut"];
-        $date_heure_fin = $_POST["date_heure_fin"];
-        $plaque = $_POST["plaque"];
-        
-        // Conversion des datetime-local en variables distinctes
-        $date_debut = substr($date_heure_debut, 0, 10);
-        $heure_debut = (int)substr($date_heure_debut, 11, 5);
-        
-        $date_fin = substr($date_heure_fin, 0, 10);
-        $heure_fin =(int)substr($date_heure_fin, 11, 5);
+include('connexion.php') ; 
 
-        
-        if (!empty($plaque) && !preg_match("/^[0-9]{4}[A-Za-z]{2}[0-9]{2}$/", $plaque)) {
-            echo '<script>alert("La plaque d\'immatriculation ne respecte pas le format attendu (1234AB12).");</script>';
-            echo "<p></p>";
-            exit; 
-        }
+    //recuperation des entrées 
+    $method = $_SERVER['REQUEST_METHOD'];  //stokage de la methode
+    $date_heure_debut = $_GET['date_heure_debut']; // recuperation de la date et heure de debut
+    $date_heure_fin = $_GET['date_heure_fin']; //recuperation de la date et heure de fin
+    $plaque = $_GET['plaque']; //recuperation de la plaque
+    $dateactuelle = date('Y-m-d H:i') ;  //date actuelle pour verifier les dates
 
-        //Requete SQL de la gloire
-        $sql = "SELECT * FROM parking WHERE 1   " ; 
-        //SELECT * FROM `parking` WHERE DATE_FORMAT(date, '%Y-%m-%d %H:%i' )>='2023-08-28 08:35'
-      
-        if (!empty($date_debut) && !empty($date_fin)) {
-                $sql .= " AND DATE_FORMAT(date, '%Y-%m-%dT%H:%i' ) >= '$date_heure_debut' AND DATE_FORMAT(date, '%Y-%m-%dT%H:%i' ) <= '$date_heure_fin'";
-        }
+    // Vérification de la methode
+    if($method != 'GET')
+    {
+        http_response_code(405) ; 
+        echo json_encode("erreur sur la methode") ; 
+        exit;
+    }
 
-        if (!empty($plaque)) {
-            $sql .= " AND plaque = '$plaque'";
-        }
+    //Verifiaction du format de la plaque 
+    if (!empty($plaque) && !preg_match("/^[0-9]{4}[A-Za-z]{2}[0-9]{2}$/", $plaque)) 
+    {
+        http_response_code(400) ; 
+        echo json_encode("Veuillez entrer une plaque valide EX :1234AB01");
+        exit; 
+    }
+
+    //Verification des dates 
+
+    $pres = "" ; 
+    if((!empty($date_heure_debut) && !empty($date_heure_fin)))
+    {
+        $pres = "1" ;  
+    }  
+
+
+    ////Verifié que la date de debut soit superieur a la date de fin
+    if(!empty($pres) && ($date_heure_debut > $date_heure_fin))
+    {
+        http_response_code(400); 
+        echo json_encode("La date de debut ne doit pas etre superieur a la date de fin") ; 
+        exit ; 
+    }
+
+    //Verifié que la date de fin soit superieur a la date actuelle
+    if(!empty($pres) && ($date_heure_fin > $dateactuelle ))
+    {
+        http_response_code(400); 
+        echo json_encode("La date de fin ne doit pas etre superieur a la date actuelle") ; 
+        exit ; 
+    }    
+
+    //Verifié que la date de debut soit superieur a la date actuelle
+    if(!empty($pres)&& ($date_heure_debut > $dateactuelle))
+    {
+        http_response_code(400); 
+        echo json_encode("La date de debut ne doit pas etre superieur a la date actuelle") ;  
+        exit ; 
+    }
+
+    //Constitution de la requete SQL 
+    $sql = "SELECT * FROM parking WHERE 1   " ;       
+
+    //Verification des dates et concatenation de la requete SQL
+    if (!empty($date_heure_debut)&& (empty($date_heure_fin))) 
+    {
+        $sql .= " AND date >= '$date_heure_debut'";
+    }
+    elseif(!empty($date_heure_fin)&& (empty($date_heure_debut)))
+    {
+        $sql .= " AND date <= '$date_heure_fin'";
+    }
+    elseif(!empty($pres))
+    {
+        $sql .= " AND date >= '$date_heure_debut' AND date <= '$date_heure_fin'";
+    }
+
+
+    //Vérification de l'existance de la plaque et ensuite ajout la requete correspondante
+    if (!empty($plaque)) {
+        $sql .= " AND plaque = '$plaque'";
+    }
         
-        $sql .= " ORDER BY date";
+    $sql .= " ORDER BY date"; //Ajout de l'ordonnement par ordre croissant
+
+    //Affichage du tableau json des resultats de la requete 
+    try
+    {
+        http_response_code(200) ;      
         $resultat = $connexion->query($sql);
-
-        if ($resultat) {
-            $tableauResultats = array();
-            while ($ligne = $resultat->fetch_assoc()) {
-                $tableauResultats[] = $ligne;
-            }
-            echo json_encode($tableauResultats);
+        $tableauResultats = array();
+        while ($ligne = $resultat->fetch_assoc()) { // stockage des ligne dans $tableauResultats[]
+            $tableauResultats[] = $ligne;
         }
-        else
-        {
-            echo "0" ; 
-        }
+        echo json_encode($tableauResultats);
+    }
+    catch(Exception $e)
+    {
+        http_response_code(500) ;
+        echo json_encode("erreur au niveau de la requete"); 
+        echo ($sql) ; 
     }
     
 ?>
